d2d25cd6952f3bacb8d208632329e24edcae6cfa
mateuszebendo <mateuszebendovp30@gmail.com>
2025-08-16 18:08:02 -0300

    feat: adiciona formulario de cadastro

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { User } from 'src/app/models/user.model';
 import { SessaoService } from 'src/app/services/sessao/sessao.service';
@@ -9,32 +9,43 @@ import { SessaoService } from 'src/app/services/sessao/sessao.service';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  public isCadastro: boolean = false;
+  public loginForm!: FormGroup;
+
   constructor(
     private router: Router,
-    private sessaoService: SessaoService
+    private sessaoService: SessaoService,
+    private formBuilder: FormBuilder
   ){}
 
-  public loginForm = new FormGroup ({
-    email: new FormControl('', [Validators.required, Validators.email]), 
-    password: new FormControl('', [Validators.required, Validators.minLength(6)])
-  });
+  ngOnInit(): void {
+    this.setupForm();
+  }
+
+  setupForm(): void{
+    this.loginForm = this.formBuilder.group({
+      email: new FormControl('', [Validators.required, Validators.email]), 
+      password: new FormControl('', [Validators.required, Validators.minLength(6)])
+    });
+  }
 
   onLoginSubmit(){
-    const formValue = this.loginForm.value;
-
-    const user: User = {
-      name: "name", 
-      email: formValue.email as string,
-      senha: formValue.password as string
-    }
-
-    this.sessaoService.salvarSessao(user);
-
-    this.router.navigateByUrl("/home");
   }
 
   onReset(){
     this.loginForm.reset();
+  }
+
+  toggleMode(){
+    this.isCadastro = !this.isCadastro; 
+
+    if(this.isCadastro){
+      this.loginForm.addControl('name', new FormControl('', [Validators.required, Validators.minLength(6)]));
+    } else {
+      this.loginForm.removeControl('name');
+    }
+
+    this.onReset();
   }
 }
