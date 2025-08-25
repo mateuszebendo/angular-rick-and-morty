4adc0ef45a9e11647474daa93cc26e0545f92013
mateuszebendo <mateuszebendovp30@gmail.com>
2025-08-24 22:14:45 -0300

    feat: adiciona opcao de navegar entre as paginas

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -8,15 +8,30 @@ import { CharacterService } from 'src/app/services/character/character.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  public currentPage: number = 1;
   public characters: Character[] = [];
 
   constructor(private characterService: CharacterService){}
 
   ngOnInit(): void{
-    this.characterService.getCharacters(1).subscribe(
+    this.getCharacters();
+  }
+
+  private getCharacters(){
+    this.characterService.getCharacters(this.currentPage).subscribe(
       (data: any) => {
         this.characters = data.results;
       }
     );
+  }
+
+  nextPage(): void {
+    this.currentPage++; 
+    this.getCharacters();
+  }
+
+  previousPage(): void {
+    this.currentPage--;
+    this.getCharacters();
   }
 }
